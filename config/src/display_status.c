a4c06523801fba4e495a188520ba5855f4680f7f
Subrat Samal <samalsubrat919@gmail.com>
2025-09-29 18:50:43 +0530

    hmmm

--- config/src/display_status.c
+++ config/src/display_status.c
@@ -1,14 +1,17 @@
-#include <device.h>
-#include <drivers/display.h>
+#include <zephyr/kernel.h>
+#include <zmk/display.h>
 #include <lvgl.h>
 
-void oled_init(const struct device *dev) {
-    display_blanking_off(dev);  // turn display on
+LV_FONT_DECLARE(lv_font_montserrat_14);
+
+int display_hello(struct zmk_display *disp) {
+    lv_obj_t *label = lv_label_create(lv_scr_act());
+    lv_label_set_text(label, "HELLO");
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+    lv_obj_update_layout(label);
+
+    lv_task_handler();
+    return 0;
 }
 
-void oled_update(const struct device *dev) {
-    lv_obj_t *label = lv_label_create(lv_scr_act());
-    lv_label_set_text(label, "Hello ZMK!");
-    lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
-    lv_task_handler();
-}
+ZMK_DISPLAY_STATUS_SCREEN(disp_hello, display_hello);
