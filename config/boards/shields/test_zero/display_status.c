249f6574354dd960ecebb970501699d7dc3af9ac
Subrat Samal <samalsubrat919@gmail.com>
2025-09-29 23:42:21 +0530

    lllll

--- config/boards/shields/test_zero/display_status.c
+++ config/boards/shields/test_zero/display_status.c
@@ -7,15 +7,11 @@
 #include <zmk/display/status_screen.h>
 #include <lvgl.h>
 
-#include <logging/log.h>
-LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
-
 lv_obj_t *zmk_display_status_screen() {
     lv_obj_t *label = lv_label_create(lv_scr_act());
-    lv_label_set_text(label, "HELLO");
+    lv_label_set_text(label, "HELLO WORLD!");
+    lv_obj_set_style_text_color(label, lv_color_white(), LV_PART_MAIN);
     lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
-    
-    LOG_INF("Custom status screen created");
     
     return label;
 }
